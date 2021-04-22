03250020391b6bed213c253da20166c444aadc55
xinqian-mit <xinchien92@gmail.com>
2021-04-22 15:07:12 -0400

    removed setup.py for cython

--- setup_AF.py
+++
@@ -1,8 +0,0 @@
-#!python3
-#cython: language_level=3
-
-from Cython.Build import cythonize
-from distutils.core import setup
-
-setup(ext_modules = cythonize('AllanFeldman.pyx'),extra_link_args=[])
-
