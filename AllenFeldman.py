958c2dddc98c5f46d5a78a04e302588774411d5a
xinqian-mit <xinqian-mit@github.com>
2022-06-19 20:06:12 +0800

    more general codes

--- AllenFeldman.py
+++ AllenFeldman.py
@@ -4,129 +4,65 @@ from phonopy.harmonic.force_constants import similarity_transformation
 from numba import jit,njit,prange
 from API_phonopy import phonopyAtoms_to_aseAtoms,aseAtoms_to_phonopyAtoms
 
-def get_Va_ijbc(phonon):
-    """
-    obtain the velocity operator
-    (Va)_ij^bc = (Ria-Rja)*\Phi_ij^bc/sqrt(Mi*Mj)
-    """
-    atoms = phonopyAtoms_to_aseAtoms(phonon.get_primitive()) # supercell and primitive cell should be the same.
-    masses = atoms.get_masses()
-    Natom = atoms.get_global_number_of_atoms()
-    
-    Rvec_atoms = np.zeros((Natom,Natom,3))
-    
-    for i in range(Natom):
-        for j in range(i):
-            xij,yij,zij,rij = find_nearest(atoms,i,j)
-            Rvec_atoms[i,j] = np.array([xij,yij,zij])
-            
-    Phi = phonon.get_force_constants() # get force constants.
-    
-    Vx = np.zeros((Natom,Natom,3,3))
-    Vy = np.zeros((Natom,Natom,3,3))
-    Vz = np.zeros((Natom,Natom,3,3))
-    
-    for i in range(Natom):
-        for j in range(i):
-            Vx_ij = -Rvec_atoms[i,j,0]*Phi[i,j]/np.sqrt(masses[i]*masses[j])
-            Vy_ij = -Rvec_atoms[i,j,1]*Phi[i,j]/np.sqrt(masses[i]*masses[j])
-            Vz_ij = -Rvec_atoms[i,j,2]*Phi[i,j]/np.sqrt(masses[i]*masses[j])
-            
-            Vx[i,j]=Vx_ij
-            Vx[j,i]=-Vx_ij.T
-            Vy[i,j]=Vy_ij
-            Vy[j,i]=-Vy_ij.T         
-            Vz[i,j]=Vz_ij
-            Vz[j,i]=-Vz_ij.T
-        
-    # flatten the velocity operator
-    # Va_(i,j,b,c) to Va_[(ib),(jc)]
-    
-    Vx_flat = np.reshape(Vx.transpose(0,2,1,3),(Natom*3,Natom*3))
-    Vy_flat = np.reshape(Vy.transpose(0,2,1,3),(Natom*3,Natom*3))
-    Vz_flat = np.reshape(Vz.transpose(0,2,1,3),(Natom*3,Natom*3))
-    
-    return Vx_flat,Vy_flat,Vz_flat
 
 
-def get_velmat_modepairs(freqs,eigvecs,Vx_flat,Vy_flat,Vz_flat,factor=VaspToTHz):
-    """
-       va_mn = em*(Va*en) computes the pairwise velocity operators.
-       for phonon gas model, va_mn = va_n*\delta_mn
-    """
-    freqs = np.reshape(np.abs(freqs),(1,len(freqs))) # some very small negative frequencies
+def get_dq_dynmat_q(phonon,q,dq=1e-5):
+    #phonon._set_group_velocity()
+    #_gv = phonon._group_velocity
+    #ddm = _gv._get_dD(q)
+    _reciprocal_lattice_inv = phonon.get_primitive().cell
+    
+    
+    #dirx = np.dot(_reciprocal_lattice,np.array([1,0,0]))
+    dq0 = dq*np.array([1,0,0])           
+    dm_p0 = phonon.get_dynamical_matrix_at_q(q + dq0)
+    dm_m0 = phonon.get_dynamical_matrix_at_q(q - dq0)
+    ddm0 = (dm_p0 - dm_m0)/dq/2.0
+    
+    dq1 = dq*np.array([0,1,0])        
+    dm_p1 = phonon.get_dynamical_matrix_at_q(q + dq1)
+    dm_m1 = phonon.get_dynamical_matrix_at_q(q - dq1)    
+    ddm1 = (dm_p1 - dm_m1)/dq/2.0
+    
+    dq2 = dq*np.array([0,0,1])   
+    dm_p2 = phonon.get_dynamical_matrix_at_q(q + dq2)
+    dm_m2 = phonon.get_dynamical_matrix_at_q(q - dq2)
+    ddm2 = (dm_p2 - dm_m2)/dq/2.0    
+    
+    ddmx = ddm0*_reciprocal_lattice_inv[0,0] + ddm1*_reciprocal_lattice_inv[1,0] + ddm2*_reciprocal_lattice_inv[2,0]
+    ddmy = ddm0*_reciprocal_lattice_inv[0,1] + ddm1*_reciprocal_lattice_inv[1,1] + ddm2*_reciprocal_lattice_inv[2,1]
+    ddmz = ddm0*_reciprocal_lattice_inv[0,2] + ddm1*_reciprocal_lattice_inv[1,2] + ddm2*_reciprocal_lattice_inv[2,2]
+    
+    ddm = [ddmx,ddmy,ddmz]
+    return ddm
+    
+
+def get_velmat_modepairs_q(phonon,q,factor=VaspToTHz):
+    freqs,eigvecs = phonon.get_frequencies_with_eigenvectors(q)
+    ddm = get_dq_dynmat_q(phonon,q) # three components.
+    
     sqrt_fnfm = np.sqrt(freqs.T*freqs)
-    temp_vx = np.dot(Vx_flat,eigvecs)
-    vx_modepairs = np.dot(eigvecs.T,temp_vx)/sqrt_fnfm/2*factor**2 # ATHz
     
-    temp_vy = np.dot(Vy_flat,eigvecs)
-    vy_modepairs = np.dot(eigvecs.T,temp_vy)/sqrt_fnfm/2*factor**2 # ATHz
+    temp_vx = np.dot(ddm[0],eigvecs)
+    vx_modepairs = np.dot(eigvecs.conjugate().T,temp_vx)/sqrt_fnfm/2/np.pi*factor**2 # ATHz
     
-    temp_vz = np.dot(Vz_flat,eigvecs)
-    vz_modepairs = np.dot(eigvecs.T,temp_vz)/sqrt_fnfm/2*factor**2 # ATHz
+    temp_vy = np.dot(ddm[1],eigvecs)
+    vy_modepairs = np.dot(eigvecs.conjugate().T,temp_vx)/sqrt_fnfm/2/np.pi*factor**2 # ATHz
+    
+    temp_vz = np.dot(ddm[2],eigvecs)
+    vz_modepairs = np.dot(eigvecs.conjugate().T,temp_vz)/sqrt_fnfm/2/np.pi*factor**2 # ATHz
     
     return vx_modepairs,vy_modepairs,vz_modepairs
 
 
-    
-def find_nearest(atoms,i,j): # under periodic condition, find the true distance
-    #for 3-dim case
-
-    distance=atoms.get_distance(i,j,vector=True)
-    xdc=distance[0]
-    ydc=distance[1]
-    zdc=distance[2]
-
-    rmin=10000.0
-
-    rv=atoms.cell
-
-    if (atoms.cell.shape[0] ==3):
-        #set
-        xcdi=xdc-2.0*rv[0,0]
-        ycdi=ydc-2.0*rv[1,0]
-        zcdi=zdc-2.0*rv[2,0]
-
-        for ii in [-1,0,1]:
-            xcdi=xcdi+rv[0,0]
-            ycdi=ycdi+rv[1,0]
-            zcdi=zcdi+rv[2,0]
-
-            xcdj=xcdi-2.0*rv[0,1]
-            ycdj=ycdi-2.0*rv[1,1]
-            zcdj=zcdi-2.0*rv[2,1]
-
-            for jj in [-1,0,1]:
-                xcdj=xcdj+rv[0,1]
-                ycdj=ycdj+rv[1,1]
-                zcdj=zcdj+rv[2,1]
-                xcrd = xcdj - 2.0*rv[0,2]
-                ycrd = ycdj - 2.0*rv[1,2]
-                zcrd = zcdj - 2.0*rv[2,2]
-
-                for kk in [-1,0,1]:
-                    xcrd = xcrd + rv[0,2]
-                    ycrd = ycrd + rv[1,2]
-                    zcrd = zcrd + rv[2,2]
-                    r = xcrd*xcrd + ycrd*ycrd + zcrd*zcrd
-                    if (r<rmin):
-                        rmin = r
-                        xdc = xcrd
-                        ydc = ycrd
-                        zdc = zcrd
-    
-    return xdc,ydc,zdc,rmin    
-
-
-
 @njit
 def delta_lorentz( x, width):
-    return (width)/(x*x + width*width)
+    return (width)/(x*x + width*width)/np.pi
 
 
 @njit 
 def double_lorentz(w1,w2,width1,width2):
-    return (width1+width2)/((w1-w2)*(w1-w2)+(width1+width2)**2)
+    return (width1+width2)/((w1-w2)*(w1-w2)+(width1+width2)**2)/np.pi
 
     
 @njit
@@ -156,7 +92,7 @@ def calc_Diff(freqs,vx_modepairs,vy_modepairs,vz_modepairs,LineWidth=1e-2,factor
             wr = freqs[r]*2*np.pi
             wsr_avg = (ws+wr)/2.0
             tau_sr = double_lorentz(ws,wr,LineWidth,LineWidth) # THz^-1                                        
-            Diff_s -= wsr_avg**2*(vx_modepairs[s,r]*vx_modepairs[r,s]+vy_modepairs[s,r]*vy_modepairs[r,s]+vz_modepairs[s,r]*vz_modepairs[r,s]).real*tau_sr/3.0 #A^2THz^2*THz-1 = A^2THz       
+            Diff_s += wsr_avg**2*np.abs((vx_modepairs[s,r]*vx_modepairs[r,s]+vy_modepairs[s,r]*vy_modepairs[r,s]+vz_modepairs[s,r]*vz_modepairs[r,s]).real)*tau_sr/3.0       
         Diffusivity[s] = Diff_s*(A2m**2*THz2Hz)/(ws**2) #A^2THz^4/THz^2*THz-1 = A^2THz
     
     
--- Example_Scripts/SW_aSi_AFModel/AF_aSi.py
+++ Example_Scripts/SW_aSi_AFModel/AF_aSi.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
 
 import matplotlib
 #matplotlib.use('agg') # This line is necessary for non-gui linux systems
@@ -16,6 +18,9 @@ import API_phonopy as api_ph
 import AllenFeldman as AF
 
 
+# In[2]:
+
+
 mesh = [1,1,1] # at only gamma point.
 T = 300
 Gamma = np.array([0,0,0])
@@ -25,9 +30,7 @@ phonon = phonopy.load(supercell_matrix=[1,1,1],primitive_matrix='auto',
                      force_constants_filename='FORCE_CONSTANTS') # load the force constants.
 
 
-
-Vx,Vy,Vz = AF.get_Va_ijbc(phonon)
-
+# In[3]:
 
 
 phonon.run_mesh(mesh, is_gamma_center=True, 
@@ -36,7 +39,7 @@ phonon.run_mesh(mesh, is_gamma_center=True,
 qpoints, weights, frequencies, eigvecs = phonon.get_mesh()
 eigvecs = eigvecs[0] # at Gamma Point
 freqs = frequencies[0]
-
+Broadening = broad_factor*np.mean(np.diff(freqs))
 
 Cmodes = api_ph.mode_cv(T,freqs)
 
@@ -44,19 +47,25 @@ Vol = phonon.get_supercell().get_volume()*1e-30
    
 
 
-Vx_flat,Vy_flat,Vz_flat = AF.get_Va_ijbc(phonon)
-vx_modepairs,vy_modepairs,vz_modepairs = AF.get_velmat_modepairs(freqs,eigvecs,Vx_flat,Vy_flat,Vz_flat)
-Broadening = broad_factor*np.mean(np.diff(freqs))
+# In[4]:
+
+
+vx_modepairs,vy_modepairs,vz_modepairs = AF.get_velmat_modepairs_q(phonon,Gamma)
 Diffusivities = AF.calc_Diff(freqs,vx_modepairs,vy_modepairs,vz_modepairs,LineWidth=Broadening)
-
-
 plt.semilogy(freqs[3:],Diffusivities[3:],'o')
 plt.xlabel('Frequency (THz)')
 plt.ylabel('Diffusivity ($m^2$/s)')
 
 
+# In[5]:
+
+
 k = np.sum(Cmodes[3:]*Diffusivities[3:]/Vol)
 print(k)
+
+
+# In[ ]:
+
 
 
 
